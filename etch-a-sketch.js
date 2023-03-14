dca325fe75cf4ac5b3d7cc38e76842752bd1f3df
Ricky <rzh4321@gmail.com>
2023-03-14 17:57:51 -0400

    added mouseOver event that generates random color to each square

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,18 @@
 function createGrid(rows, columns) {
-    let row;
     for (let r = 0; r < rows; ++r) {
-        row = document.createElement('div');
+        let row = document.createElement('div');
         row.style.cssText = 'display: flex;';
         for (let c = 0; c < columns; ++c) {
             let div = document.createElement('div');
             div.style.width = '40px';
             div.style.height = '40px';
-            //const randomColor = Math.floor(Math.random()*16777215).toString(16);
-            div.style.backgroundColor = null;
             div.style.border = '2px black solid';
+            div.addEventListener('mouseover', function() {
+                if (div.style.backgroundColor == '') {
+                    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+                    div.style.backgroundColor = '#' + randomColor;
+                }
+            });
             row.append(div);
             container.append(row);
         }
