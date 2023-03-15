e886ae5488b505307c28f12cf014cff9df31c44e
Ricky <rzh4321@gmail.com>
2023-03-14 22:24:35 -0400

    set dimensions to fixed number

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,11 +2,12 @@ function createGrid(rows, columns) {
     for (let r = 0; r < rows; ++r) {
         let row = document.createElement('div');
         row.style.cssText = 'display: flex;';
+        row.style.flexGrow = 1;
         for (let c = 0; c < columns; ++c) {
             let div = document.createElement('div');
             div.style.width = '40px';
-            div.style.height = '40px';
             div.style.border = '2px black solid';
+            div.style.flexGrow = 1;
             div.addEventListener('mouseover', function() {
                 if (div.style.backgroundColor == '') {
                     const randomColor = Math.floor(Math.random()*16777215).toString(16);
@@ -14,9 +15,22 @@ function createGrid(rows, columns) {
                 }
             });
             row.append(div);
-            container.append(row);
         }
+        let container = document.body.querySelector('.container');
+        container.append(row);
     }
 }
 
 createGrid(16,16);
+button.addEventListener('click', function() {
+    let container = document.querySelector('.container');
+    container.remove();
+    let cont = document.createElement('div');
+    cont.classList.add('container');
+    body.append(cont);
+    let rows = prompt('enter number of rows:', 16);
+    while (+rows > 100) rows = prompt('enter number of rows:', 16);
+    let columns = prompt('enter number of columns:', 16);
+    while (+columns > 100) columns = prompt('enter number of columns:', 16);
+    createGrid(rows, columns);
+})
